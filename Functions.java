f849861d9e557b43245739e1fce34f2ecad46d17
OmkarBagav <110346724+OmkarBagav@users.noreply.github.com>
2023-10-24 22:24:01 +0530

    Solved 4 questions instead of 2

--- Functions.java
+++ Functions.java
@@ -1,42 +1,98 @@
 import java.util.Scanner;
 
 public class Functions {
-    public static void main(String[] args) {       // Scanner sc=new Scanner(System.in);
-        //System.out.println("Enter Radius of circle");
-        //float rad= sc.nextFloat();
-        //System.out.println("The area of circle is "+circlearea(rad));
+    /*Q1- Find area of circle using your function
+    public static void main(String[] args) {
 
+     Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Radius of circle");
+    float rad= sc.nextFloat();
+    System.out.println("The area of circle is "+circlearea(rad));
 
-    Scanner sc=new Scanner(System.in);
-        System.out.println("Enter no 1");
-        int n1= sc.nextInt();
-        System.out.println("Enter no 2");
-        int n2= sc.nextInt();
-        System.out.println("Enter no 3");
-        int n3= sc.nextInt();
-     max(n1,n2,n3);
+}
+    public static float  circlearea (float r) {
+     return (float)(22/7)*r*r;
 }
 
 
+     */
 
+    /*Q2- Find max of 3 nos using a functions
 
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter no 1");
+            int n1 = sc.nextInt();
+            System.out.println("Enter no 2");
+            int n2 = sc.nextInt();
+            System.out.println("Enter no 3");
+            int n3 = sc.nextInt();
 
-    //public static float  circlearea (float r) {
-   // return (float)(22/7)*r*r;
-
-    public static void max(int n1,int n2,int n3){
-        if (n1> n2 ){
-            if (n1> n3){
-                System.out.println(n1);            }
-        } else if (n2>n1) {
-            if (n2 > n3) {
-                System.out.println(n2);            }
+            String result = max(n1, n2, n3);
+            System.out.println(result);
         }
-        else{
-            System.out.println(n3);        }
 
+        public static String max(int num1, int num2, int num3) {
+            int max;
+            if (num1 >= num2 && num1 >= num3) {
+                max = num1;
+            } else if (num2 >= num1 && num2 >= num3) {
+                max = num2;
+            } else {
+                max = num3;
+            }
+
+            String message = "The maximum of " + num1 + ", " + num2 + ", and " + num3 + " is: " + max;
+            return message;
+        }
     }
 
 
+     */
+
+
+
+
+    /* Q3- Find max of 3 nos using a functions
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int n2 = sc.nextInt();
+
+    int total = Sum(n1, n2);
+
+        System.out.println("Sum is: " + total);
+}
+
+    public static int Sum(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+     */
+
+    /*Q4 write a fxn to check even or odd number
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n1 = sc.nextInt();
+        if (isEven(n1)) {
+            System.out.println(n1 + " is even.");
+        } else {
+            System.out.println(n1 + " is odd.");
+        }
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+
+     */
 
 }
+
+
